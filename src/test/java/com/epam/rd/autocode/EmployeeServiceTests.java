09d0f5a86a2ce5369258e154d39cee23b8f73151
thejerome <thejerome@mail.ru>
2020-07-09 10:17:12 +0300

    Tests to single class

--- src/test/java/com/epam/rd/autocode/EmployeeServiceTests.java
+++ src/test/java/com/epam/rd/autocode/EmployeeServiceTests.java
@@ -19,6 +19,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -391,6 +392,32 @@ public class EmployeeServiceTests {
     private Employee employeeFrom(final Path json) {
         try {
             return Employee.Parser.parseJson(FileUtils.readFileToString(json.toFile(), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Test
+    public void testNoChangesToReferenceClasses() throws Exception {
+        final ImmutableMap<String, String> src2Ref = ImmutableMap.<String, String>builder()
+                .put("src/main/java/com/epam/rd/autocode/domain/Department.java", "src/test/resources/ref/Department.java")
+                .put("src/main/java/com/epam/rd/autocode/domain/Employee.java", "src/test/resources/ref/Employee.java")
+                .put("src/main/java/com/epam/rd/autocode/domain/FullName.java", "src/test/resources/ref/FullName.java")
+                .put("src/main/java/com/epam/rd/autocode/domain/Position.java", "src/test/resources/ref/Position.java")
+                .put("src/main/java/com/epam/rd/autocode/ConnectionSource.java", "src/test/resources/ref/ConnectionSource.java")
+                .put("src/main/resources/init-ddl.sql", "src/test/resources/ref/init-ddl.sql")
+                .put("src/main/resources/init-dml.sql", "src/test/resources/ref/init-dml.sql")
+                .build();
+
+        src2Ref.forEach((src, ref) -> assertSourceEqualsReference(Paths.get(src), Paths.get(ref)));
+    }
+
+    private void assertSourceEqualsReference(final Path src, final Path ref) {
+        try {
+            final List<String> refLines = Files.readAllLines(src);
+            final List<String> srcLines = Files.readAllLines(ref);
+            assertEquals(src + " was modified", refLines, srcLines);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
